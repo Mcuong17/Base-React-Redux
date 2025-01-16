f2cafa3dccd465c1876a7d632fd475625a08e04e
Mcuong17 <manhcuong7381@gmail.com>
2025-01-16 23:53:30 +0700

    bai 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,11 +8,22 @@ class MyComponent extends React.Component {
         age:23
       }
 
+    handleClick(event) {
+        console.log(this.state);
+    }
+    handeOnmouseOver(event) {
+        console.log('====================================');
+        console.log(event.pageX);
+        console.log('====================================');
+    }  
+
     /* JSX */
     render() {
         return (
             <div>
                 My name is {this.state.name} I from {this.state.address}
+                <button onClick={this.handleClick}>Click Me</button>
+                <button onMouseOver={this.handeOnmouseOver}>Hover Me</button>
             </div>
         );
     }
