f34ebbdba24a19238eb2cd0dc5ac08a7713a434c
Mcuong17 <manhcuong7381@gmail.com>
2025-02-06 16:36:29 +0700

    bai 35

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './DisplayInfor.scss'
 
 //Stateless: ko có state, stateful: có state
@@ -42,10 +42,19 @@ const DisplayInfor = (props) => {
     const { listUser } = props
     //DRY: Dont repeat your seft
     //props => properties
+    const [isShowHideListUser, setShowHideListUser] = useState(true);
+
+    const handleShowHideListUser = () => {
+        setShowHideListUser(!isShowHideListUser)
+    }
+
     return (
         <>
+        <div>
+            <span onClick={() => handleShowHideListUser()}>{isShowHideListUser ? 'Hide list user' : 'Show list user'}</span>
+        </div>
         <div className="display-infor-container"> {/* Khai báo class cha để dùng scss ko bị trùng lặp */}
-            {true &&
+            {isShowHideListUser &&
                 <div>
                     {listUser.map((user) => {
                         return (
