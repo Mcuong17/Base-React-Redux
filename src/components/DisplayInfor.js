133cd6d478cea2e7ffd5b786f48ce35edd3cd265
Mcuong17 <manhcuong7381@gmail.com>
2025-02-06 16:15:02 +0700

    bai 34

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,73 +1,69 @@
 import React from "react";
 import './DisplayInfor.scss'
-import logo from '../logo.svg'
 
-class DisplayInfor extends React.Component {
+//Stateless: ko có state, stateful: có state
+// class DisplayInfor extends React.Component {
 
-    constructor(props) {
-        console.log('>>> call contructor',0)
-        super(props);
-        this.state = {
-            hideUser: true
-        }
-    }
+//     render() {
+//         console.log('>> call me render')
+//         // distructuring props
+//         const { listUser } = this.props
+//         //DRY: Dont repeat your seft
+//         //props => properties
+//         return (
+//             <>
+//             <div className="display-infor-container"> {/* Khai báo class cha để dùng scss ko bị trùng lặp */}
+//                 <div onClick={(event) => { this.handleShowHideUser(event) }}>{this.state.hideUser ? 'Hide' : 'Show'} list user</div>
+//                 {true &&
+//                     <div>
+//                         {listUser.map((user) => {
+//                             return (
+//                                 <div>
+//                                     <div key={user.id} className={+user.age > 18 ? 'green' : 'red'}>
+//                                         <div>My name {user.name}</div>
+//                                         <div>My age {user.age}</div><hr />
+//                                     </div>
+//                                     <button onClick={() => this.props.handleDeleteUser(user.id) }>Delete</button>
+//                                 </div>
+//                             )
+//                         }
+//                         )}
+//                     </div>
+//                 }
+//             </div>
+//             </>
+//         )
+//     }
+// }
 
-    componentDidMount() {
-        //Hàm này sẽ chạy sau khi mà đã có dữ liệu trong dom
-        console.log('>> call me component did mount', 1)
-        setTimeout(() => {
-            document.title = 'test'
-        }, 3000);
-    }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        // nếu không truyền đầu vào thì sẽ ko chạy
-        //chạu khi có newState, newProps
-        if(this.props.listUser != prevProps) {
-            console.log('>> call me component did update', this.props, prevProps)
-            if(this.props.listUser.length === 5) {
-                console.log('You have 5 user')
-            }
-        }
-
-    }
-
-    handleShowHideUser = () => {
-        this.setState({
-            hideUser: !this.state.hideUser
-        })
-    }
-
-    render() {
-        console.log('>> call me render')
-        // distructuring props
-        const { listUser } = this.props
-        //DRY: Dont repeat your seft
-        //props => properties
-        return (
-            <>
-            <div className="display-infor-container"> {/* Khai báo class cha để dùng scss ko bị trùng lặp */}
-                <div onClick={(event) => { this.handleShowHideUser(event) }}>{this.state.hideUser ? 'Hide' : 'Show'} list user</div>
-                {this.state.hideUser &&
-                    <div>
-                        {listUser.map((user) => {
-                            return (
-                                <div>
-                                    <div key={user.id} className={+user.age > 18 ? 'green' : 'red'}>
-                                        <div>My name {user.name}</div>
-                                        <div>My age {user.age}</div><hr />
-                                    </div>
-                                    <button onClick={() => this.props.handleDeleteUser(user.id) }>Delete</button>
+const DisplayInfor = (props) => {
+    // distructuring props
+    const { listUser } = props
+    //DRY: Dont repeat your seft
+    //props => properties
+    return (
+        <>
+        <div className="display-infor-container"> {/* Khai báo class cha để dùng scss ko bị trùng lặp */}
+            {true &&
+                <div>
+                    {listUser.map((user) => {
+                        return (
+                            <div>
+                                <div key={user.id} className={+user.age > 18 ? 'green' : 'red'}>
+                                    <div>My name {user.name}</div>
+                                    <div>My age {user.age}</div><hr />
                                 </div>
-                            )
-                        }
-                        )}
-                    </div>
-                }
-            </div>
-            </>
-        )
-    }
+                                <button onClick={() => props.handleDeleteUser(user.id) }>Delete</button>
+                            </div>
+                        )
+                    }
+                    )}
+                </div>
+            }
+        </div>
+        </>
+    )
 }
 
 export default DisplayInfor
