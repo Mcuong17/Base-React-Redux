36675900f5b791510b910e63eef85c172295849d
Mcuong17 <manhcuong7381@gmail.com>
2025-02-13 00:19:35 +0700

    bai 46

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
-import React from "react";
 
-class App extends React.Component {
 
- 
 
-  // JSX
-  render() {
-    return (
-      <div>
-        Hello World
-        <MyComponent />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="app-container">
+      Hello world
+    </div>
+  )
 }
 export default App;
